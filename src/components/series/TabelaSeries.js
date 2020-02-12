2e51315fb432c0d0dd6cfe8294a8abb7beb82744
Sid5488 <guilhermenoobsaibot@gmail.com>
2020-02-12 16:51:06 -0300

    Inserção no banco de dados

---
+++ src/components/series/TabelaSeries.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+const TabelaHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Lançamento</th>
+                <th>Temporada</th>
+                <th>Sinopse</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TabelaBody = (props) => {
+    return (
+        <tbody>
+            {props.series.map(serie => {
+                return (
+                    <tr key={serie.id}>
+                        <td>{serie.nome}</td>
+                        <td>{serie.ano_lancamento}</td>
+                        <td>{serie.temporadas}</td>
+                        <td>{serie.sinopse}</td>
+                    </tr>
+                );
+            })}
+        </tbody>
+    );
+}
+
+class TabelaSeries extends Component {
+    render() {
+        const { series } = this.props;
+
+        return (
+            <div>
+                <div className="list">
+                    <table>
+                        <TabelaHead />
+                        <TabelaBody series={series} />
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TabelaSeries;
